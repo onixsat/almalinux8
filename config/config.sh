9c1d752eac784b485ffc2de3bd6fde3b78427d6c
onixsat <163943278+onixsat@users.noreply.github.com>
2024-10-13 00:30:48 +0100

    Delete config directory

--- config/config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-  password="Password+2024"
-  sshport="2022"
-  domain="encpro.pt"
-  hostname="srv.encpro.pt"
-  ns1="ns1.encpro.pt"
-  ns2="ns2.encpro.pt"
-  ip="135.125.183.142"
